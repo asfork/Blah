96d6d807104cc2971523aa1f7b7283fa62a129db
Steve Zhang <stevzhg@gmail.com>
2015-09-22 23:52:07 +0800

    create material design theme

---
+++ app/src/main/java/com/v2cc/im/blah/FragmentTest.java
@@ -0,0 +1,26 @@
+package com.v2cc.im.blah;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Steve ZHANG (stevzhg@gmail.com)
+ * 2015/9/22.
+ * If it works, I created this. If not, I didn't.
+ */
+public class FragmentTest extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_test, container, false);
+    }
+
+    @Override
+    public void onRefresh() {
+
+    }
+}
---
+++ app/src/main/java/com/v2cc/im/blah/ViewPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.v2cc.im.blah;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Steve ZHANG (stevzhg@gmail.com)
+ * 2015/9/22.
+ * If it works, I created this. If not, I didn't.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private String[] mTitles;
+    private List<Fragment> mFragments;
+
+    public ViewPagerAdapter(FragmentManager fm, String[] mTitles, List<Fragment> mFragments) {
+        super(fm);
+        this.mTitles = mTitles;
+        this.mFragments = mFragments;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+}
