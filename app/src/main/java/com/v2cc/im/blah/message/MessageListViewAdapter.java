55be389da41adee3228efcace06f3fda79cd44bd
Steve Zhang <stevzhg@gmail.com>
2015-10-17 11:48:52 +0800

    modify files and classes name to make codes easy to read

--- app/src/main/java/com/v2cc/im/blah/message/MessageListViewAdapter.java
+++ app/src/main/java/com/v2cc/im/blah/message/MessageListViewAdapter.java
@@ -53,10 +53,10 @@ public class MessageListViewAdapter extends BaseAdapter {
         if (convertView == null) {
             holder = new ViewHolder();
             convertView = inflater.inflate(R.layout.item_message_list, null);
-            holder.rl_chat_input = (RelativeLayout) convertView.findViewById(R.id.rl_chat_input);
-            holder.rl_chat_output = (RelativeLayout) convertView.findViewById(R.id.rl_chat_output);
-            holder.tv_content_input = (TextView) convertView.findViewById(R.id.tv_chat_input);
-            holder.tv_content_output = (TextView) convertView.findViewById(R.id.tv_chat_output);
+            holder.rl_chat_send = (RelativeLayout) convertView.findViewById(R.id.rl_chat_send);
+            holder.rl_chat_receive = (RelativeLayout) convertView.findViewById(R.id.rl_chat_receive);
+            holder.tv_content_send = (TextView) convertView.findViewById(R.id.tv_chat_send);
+            holder.tv_content_receive = (TextView) convertView.findViewById(R.id.tv_chat_receive);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
@@ -65,23 +65,23 @@ public class MessageListViewAdapter extends BaseAdapter {
         // 判断是接收还是发送
         if (messageBean.getSource().equals("1")) {
             // 接收信息
-            holder.rl_chat_input.setVisibility(View.GONE);
-            holder.rl_chat_output.setVisibility(View.VISIBLE);
-            holder.tv_content_output.setText(messageBean.getContent());
+            holder.rl_chat_send.setVisibility(View.GONE);
+            holder.rl_chat_receive.setVisibility(View.VISIBLE);
+            holder.tv_content_receive.setText(messageBean.getContent());
         } else if (messageBean.getSource().equals("0")) {
             // 发送信息
-            holder.rl_chat_input.setVisibility(View.VISIBLE);
-            holder.rl_chat_output.setVisibility(View.GONE);
-            holder.tv_content_input.setText(messageBean.getContent());
+            holder.rl_chat_send.setVisibility(View.VISIBLE);
+            holder.rl_chat_receive.setVisibility(View.GONE);
+            holder.tv_content_send.setText(messageBean.getContent());
         }
         return convertView;
     }
 
     private class ViewHolder {
-        private RelativeLayout rl_chat_input;// 聊天信息布局发送
-        private RelativeLayout rl_chat_output;// 聊天信息布局接收
-        private TextView tv_content_input;// 发送的文字消息展示
-        private TextView tv_content_output;// 接收的文字消息展示
+        private RelativeLayout rl_chat_send;// 聊天信息布局发送
+        private RelativeLayout rl_chat_receive;// 聊天信息布局接收
+        private TextView tv_content_send;// 发送的文字消息展示
+        private TextView tv_content_receive;// 接收的文字消息展示
     }
 
 }
